d26b77c08307777dd74d76b8ec1992b6609df5d0
Jon Skeet <jonskeet@google.com>
2018-09-21 16:39:06 +0100

    Propagate task status more reliably
    This won't end up wrapping exceptions as a call to Result does, and
    will reliably decrement a stream activity count even if a response
    indicates failure.
    
    Most of the commit is introducing a test service so we can test
    against that.
    
    Fixes #17.

---
+++ Grpc.Gcp/Grpc.Gcp.IntegrationTest/LocalServiceTest.cs
@@ -0,0 +1,47 @@
+﻿using Grpc.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grpc.Gcp.IntegrationTest
+{
+    [TestClass]
+    public class LocalServiceTest
+    {
+        [TestMethod]
+        public async Task ExceptionPropagation()
+        {
+            var server = new Server
+            {
+                Services = { TestService.BindService(new ThrowingService()) },
+                Ports = { new ServerPort("localhost", 0, ServerCredentials.Insecure) }
+            };
+            server.Start();
+            try
+            {
+                var port = server.Ports.First();
+                var invoker = new GcpCallInvoker(port.Host, port.BoundPort, ChannelCredentials.Insecure);
+
+                var client = new TestService.TestServiceClient(invoker);
+
+                await Assert.ThrowsExceptionAsync<RpcException>(async () => await client.DoSimpleAsync(new SimpleRequest()));
+            }
+            finally
+            {
+                await server.ShutdownAsync();
+            }
+        }
+
+
+        private class ThrowingService : TestService.TestServiceBase
+        {
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+            public override async Task<SimpleResponse> DoSimple(SimpleRequest request, ServerCallContext context)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+            {
+                throw new Exception("Bang");
+            }
+        }
+    }
+}
