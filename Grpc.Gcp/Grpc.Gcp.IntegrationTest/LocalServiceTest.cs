8416a38cc2ad88db712e5afe9a3686cf23eed2ff
Jon Skeet <jonskeet@google.com>
2018-09-24 08:41:51 +0100

    Refactor LocalServiceTest and add a synchronous test
    This will make it easier to add further tests

--- Grpc.Gcp/Grpc.Gcp.IntegrationTest/LocalServiceTest.cs
+++ Grpc.Gcp/Grpc.Gcp.IntegrationTest/LocalServiceTest.cs
@@ -1,6 +1,7 @@
 ﻿using Grpc.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,27 +13,47 @@ namespace Grpc.Gcp.IntegrationTest
         [TestMethod]
         public async Task ExceptionPropagation()
         {
+            await RunWithServer(
+                new ThrowingService(),
+                null,
+                async (invoker, client) => await Assert.ThrowsExceptionAsync<RpcException>(async () => await client.DoSimpleAsync(new SimpleRequest())),
+                (invoker, client) => Assert.ThrowsException<RpcException>(() => client.DoSimple(new SimpleRequest())));
+        }
+
+        /// <summary>
+        /// Starts up a service, creates a call invoker and a client, using an optional API config,
+        /// then executes the asynchronous and/or synchronous test methods provided.
+        /// </summary>
+        private static async Task RunWithServer(
+            TestService.TestServiceBase serviceImpl,
+            ApiConfig apiConfig,
+            Func<GcpCallInvoker, TestService.TestServiceClient, Task> asyncTestAction,
+            Action<GcpCallInvoker, TestService.TestServiceClient> testAction)
+        {
             var server = new Server
             {
-                Services = { TestService.BindService(new ThrowingService()) },
+                Services = { TestService.BindService(serviceImpl) },
                 Ports = { new ServerPort("localhost", 0, ServerCredentials.Insecure) }
             };
             server.Start();
             try
             {
                 var port = server.Ports.First();
-                var invoker = new GcpCallInvoker(port.Host, port.BoundPort, ChannelCredentials.Insecure);
-
+                var options = new List<ChannelOption>();
+                if (apiConfig != null)
+                {
+                    options.Add(new ChannelOption(GcpCallInvoker.ApiConfigChannelArg, apiConfig.ToString()));
+                }
+                var invoker = new GcpCallInvoker(port.Host, port.BoundPort, ChannelCredentials.Insecure, options);
                 var client = new TestService.TestServiceClient(invoker);
-
-                await Assert.ThrowsExceptionAsync<RpcException>(async () => await client.DoSimpleAsync(new SimpleRequest()));
+                await (asyncTestAction?.Invoke(invoker, client) ?? Task.FromResult(0));
+                testAction?.Invoke(invoker, client);
             }
             finally
             {
                 await server.ShutdownAsync();
             }
         }
-
 
         private class ThrowingService : TestService.TestServiceBase
         {
